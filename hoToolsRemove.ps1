c9830835204ca09ff9c1b27c5551a452c6ed3a21
Helmut.Ortmann <Helmut.Ortmann@t-online.de>
2016-05-01 20:48:45 +0200

    fixes

--- hoToolsRemove.ps1
+++ hoToolsRemove.ps1
@@ -3,11 +3,11 @@
 #################################################
 # Note: This procedure takes a long time
 #
-# $app = Get-WmiObject -Class Win32_Product `
-#                     -Filter "Name = 'hoTools'"
+# $app = Get-WmiObject -Class Win32_Product -Filter "Name = 'hoTools'"
 # $app.Uninstall()
 #
 $SoftwareName = "hoTools"
+"Deinstall $SoftwareName"
 
 # fast way to uninstall
 $uninstall32 = gci "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" | foreach { gp $_.PSPath } | ? { $_ -match $SoftwareName } | select UninstallString
@@ -16,10 +16,11 @@ $uninstall64 = gci "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" |
 if ($uninstall64) {
 $uninstall64 = $uninstall64.UninstallString -Replace "msiexec.exe","" -Replace "/I","" -Replace "/X",""
 $uninstall64 = $uninstall64.Trim()
-Write "Uninstalling..."
+Write "Uninstalling 64Bit..."
 start-process "msiexec.exe" -arg "/X $uninstall64 /qb" -Wait}
 if ($uninstall32) {
 $uninstall32 = $uninstall32.UninstallString -Replace "msiexec.exe","" -Replace "/I","" -Replace "/X",""
 $uninstall32 = $uninstall32.Trim()
-Write "Uninstalling..."
+Write "Uninstalling 32Bit..."
 start-process "msiexec.exe" -arg "/X $uninstall32 /qb" -Wait}
+"Deinstall $SoftwareName finished"
