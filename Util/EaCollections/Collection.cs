ad30c2d14fd294a1ca6483193e2cbcd8501e12b3
Helmut.Ortmann <Helmut.Ortmann@t-online.de>
2017-09-19 21:42:25 +0200

    collections order

--- Util/EaCollections/Collection.cs
+++ Util/EaCollections/Collection.cs
@@ -35,24 +35,25 @@ namespace hoTools.Utils.EaCollections
         public override bool OrderAlphabetic()
         {
             //EA.Element el = _rep.GetElementByID(diaObj.ElementID);
-            var list = from item in _item
-                let el = _rep.GetElementByID(item.Id)
-                select new {item.Id,el.Name, item.Left, item.Right, item.Top, item.Bottom, item}
+            var list = from item in _eaDia.SelObjects
+                let el = _rep.GetElementByID(item.ElementID)
+                select new {item.ElementID,el.Name, item.left, item.right, item.top, item.bottom, item}
                 into temp
                 orderby temp.Name
                 select temp;
             var llist = list.ToList();
-            int topItem = llist.Max(t => t.Top);
-            int bottomItem = llist.Min(t => t.Bottom);
-            int sum = llist.Sum(t => t.Top - t.Bottom);
+            int topItem = llist.Max(t => t.top);
+            int bottomItem = llist.Min(t => t.bottom);
+            int sum = llist.Sum(t => t.top - t.bottom);
             int diff = (topItem- bottomItem - sum) / llist.Count;
             int top = topItem;
             foreach (var item in llist)
             {
-                int itemDif = item.item.Top - item.item.Bottom;
-                item.item.Top = top - diff;
-                item.item.Bottom = item.item.Top - itemDif;
-                top = item.item.Bottom - diff;
+                int itemDif = item.item.top - item.item.bottom;
+                item.item.top = top;
+                item.item.bottom = item.item.top - itemDif;
+                top = item.item.bottom - diff;
+                item.item.Update();
             }
             _rep.ReloadDiagram(_eaDia.Dia.DiagramID);
             return true;
