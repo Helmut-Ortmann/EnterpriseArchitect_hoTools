55ee447f10b9d9c02fa737db2f6fe95c73ba2fa1
Helmut.Ortmann <Helmut.Ortmann@t-online.de>
2017-09-20 06:15:13 +0200

    horizontal ordering

--- Util/EaCollections/Collection.cs
+++ Util/EaCollections/Collection.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using hoTools.Utils.Diagram;
 
 namespace hoTools.Utils.EaCollections
@@ -47,21 +48,52 @@ namespace hoTools.Utils.EaCollections
                 orderby temp.Name
                 select temp;
             var llist = list.ToList();
-            int topItem = llist.Max(t => t.top);
-            int bottomItem = llist.Min(t => t.bottom);
-            int sum = llist.Sum(t => t.top - t.bottom);
-            int diff = (topItem- bottomItem - sum) / llist.Count;
-            int top = topItem;
-            foreach (var item in llist)
+            // estimate the direction 
+
+            bool isVertical =
+                (Math.Abs(_eaDia.SelObjects[0].top - _eaDia.SelObjects[1].top)) >
+                (Math.Abs(_eaDia.SelObjects[0].left - _eaDia.SelObjects[1].bottom)) 
+                    ? true
+                    : false;
+            if (isVertical)
             {
-                int itemDif = item.item.top - item.item.bottom;
-                item.item.top = top;
-                item.item.bottom = item.item.top - itemDif;
-                top = item.item.bottom - diff;
-                item.item.Update();
+                // vertical
+                int topItem = llist.Max(t => t.top);
+                int bottomItem = llist.Min(t => t.bottom);
+                int sum = llist.Sum(t => t.top - t.bottom);
+                int diff = (topItem - bottomItem - sum) / llist.Count;
+                int top = topItem;
+                foreach (var item in llist)
+                {
+                    int itemDif = item.item.top - item.item.bottom;
+                    item.item.top = top;
+                    item.item.bottom = item.item.top - itemDif;
+                    top = item.item.bottom - diff;
+                    item.item.Update();
+                }
+                Rep.ReloadDiagram(_eaDia.Dia.DiagramID);
+                return true;
             }
-            Rep.ReloadDiagram(_eaDia.Dia.DiagramID);
-            return true;
+            else
+            {
+                // horizontal
+                int leftItem = llist.Max(t => t.left);
+                int rightItem = llist.Min(t => t.right);
+                int sum = llist.Sum(t => t.top - t.bottom);
+                int diff = (leftItem - rightItem - sum) / llist.Count;
+                int left = leftItem;
+                foreach (var item in llist)
+                {
+                    int itemDif = item.item.left - item.item.right;
+                    item.item.left = left;
+                    item.item.right = item.item.left - itemDif;
+                    left = item.item.right - diff;
+                    item.item.Update();
+                }
+                Rep.ReloadDiagram(_eaDia.Dia.DiagramID);
+                return true;
+
+            }
         }
 
     }
