75bc13fb01b7b46645e49f8abf2deafc580e2734
Helmut.Ortmann <Helmut.Ortmann@t-online.de>
2017-09-22 21:38:47 +0200

    setup

--- Setup/Scripts/RenameMsi.bat
+++ Setup/Scripts/RenameMsi.bat
@@ -5,5 +5,5 @@ for /f "usebackq tokens=*" %%a in (`cscript "%~dp0\getmsiversion.vbs" "%~f1"`) d
 
 SET MSINAME="%~dp1%~n1 %myvar%%~x1"
 echo Renaming to %MSINAME%
-move /Y %1 %MSINAME%
+copy /Y %1 %MSINAME%
 
